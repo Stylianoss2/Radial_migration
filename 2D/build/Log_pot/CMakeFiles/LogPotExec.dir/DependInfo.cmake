1aa20d15555473784e78b81aeb0e5127de61884f
Stylianos Petrou <stylianodeon@Stylianos.localdomain>
2025-07-15 13:43:17 +0300

    Initial commit

---
+++ 2D/build/Log_pot/CMakeFiles/LogPotExec.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/stylianodeon/Radial_migration/2D/Log_pot/source/LogPot.cpp" "Log_pot/CMakeFiles/LogPotExec.dir/source/LogPot.cpp.o" "gcc" "Log_pot/CMakeFiles/LogPotExec.dir/source/LogPot.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 3D/build/Log_pot/CMakeFiles/LogPotExec.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../bin/LogPotExec"
+  "../bin/LogPotExec.pdb"
+  "CMakeFiles/LogPotExec.dir/source/LogPot.cpp.o"
+  "CMakeFiles/LogPotExec.dir/source/LogPot.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LogPotExec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
